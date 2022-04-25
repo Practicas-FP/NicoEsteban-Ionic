45499abe373d03c59ca90f5f410e1595c4130d3b
Esteban Borquez <n.a.esteban.borquez@accenture.com>
2022-04-25 08:33:41 +0200

    Searchbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,8 +10,8 @@ export class AppComponent {
     { title: 'Upcoming movies', url: '/upcoming-movies', icon: 'film' },
     { title: 'Popular movies', url: '/popular-movies', icon: 'film' },
     { title: 'Top rated movies', url: '/top-rated-movies', icon: 'film' },
-    { title: 'Log-In', url: '/log-in', icon: 'film' },
-    { title: 'Sign-In', url: '/sign-in', icon: 'film' }
+    { title: 'Log-In', url: '/log-in', icon: 'log-in' },
+    { title: 'Sign-In', url: '/sign-in', icon: 'create' }
   ];
 
   constructor() {}
